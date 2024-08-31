6510953af50e2b097f8a40ec827bba30a18fdb5d
James T <tharit.rt@gmail.com>
2024-08-31 13:39:29 +0700

    feat: add share button & share component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import styles from "./page.module.css"
 import { Article } from "../components/article"
+import { randomUUID } from "crypto"
 
 export default function Page() {
     return <div className={styles.background}>
@@ -8,21 +9,25 @@ export default function Page() {
         <p>รวมเรื่องเหลือจะเชื่อในสังคมไทย อะไรก็ได้</p>
         <div className={styles.body}>
             <Article
+                id="id-1"
                 title="น้องออนิว ขโมยลูกวัวป้า แลกรหัสเปส"
                 caption="ลั่น ส่งไม่เป็น ขอนัดรับวันเสาร์เพราะป้าไม่อยู่บ้าน"
                 type={1}
             />
             <Article
+                id="id-2"
                 title='นางแบกเพื่อไทย ตระบัดสัตย์ ไม่แคร์ เลือกผลประโยชน์  ช่าวเน็ตลั่น "จะไปสุดที่ตรงไหน"'
                 caption="พรรคประชาธิปัตย์มีมติเอกฉันท์ 34 เสียง เข้าร่วมรัฐบาลเพื่อไทย"
                 type={2}
             />
             <Article
+                id="id-3"
                 title='วันพีชตอนต่อไป ลูฟี่ตาย โซโลกับซันจิเป็นเกย์ อูซบติดหี วันพีชคือตูดของนามิ'
                 caption="อาจารย์โอดะไม่ได้กล่าว เพราะพูดไทยไม่ได้"
                 type={3}
             />
             <Article
+                id="id-4"
                 title='ครั้งแรกประวัติศาสตร์ สว.ไทย "เลือกกันเอง"'
                 caption="ชาวไทยอึ้ง แบบนี้ก็ได้หรอ"
                 type={4}
